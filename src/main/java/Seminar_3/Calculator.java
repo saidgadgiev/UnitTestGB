e3e01671413a9c79a75f3fcb04cb32c8039fb0ac
Said Gadjiev <ackusha1984@ya.ru>
2023-10-15 22:53:54 +0300

    Seminar 3

---
+++ src/main/java/Seminar_3/Calculator.java
@@ -0,0 +1,16 @@
+package Seminar_3;
+
+public class Calculator {
+    // Метод для сложения двух чисел
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Метод для деления двух чисел. Генерирует исключение, если делитель равен нулю.
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Делитель не может быть равен нулю");
+        }
+        return (double) a / b;
+    }
+}
